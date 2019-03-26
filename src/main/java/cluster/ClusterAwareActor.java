da25d0cbefe4cfaccd5eadc496b2e12830f5b9c2
Hugh McKee <mckee.hugh@gmail.com>
2019-03-26 14:45:28 -0400

    fix srcipt dir location, add FST serialization

--- src/main/java/cluster/ClusterAwareActor.java
+++ src/main/java/cluster/ClusterAwareActor.java
@@ -10,6 +10,7 @@ import akka.cluster.MemberStatus;
 import scala.concurrent.duration.Duration;
 import scala.concurrent.duration.FiniteDuration;
 
+import java.io.Serializable;
 import java.util.concurrent.TimeUnit;
 
 class ClusterAwareActor extends AbstractLoggingActor {
@@ -21,8 +22,8 @@ class ClusterAwareActor extends AbstractLoggingActor {
     public Receive createReceive() {
         return receiveBuilder()
                 .matchEquals("tick", s -> tick())
-                .matchEquals("ping", p -> ping())
-                .matchEquals("pong", p -> pong())
+                .match(Message.Ping.class, this::ping)
+                .match(Message.Pong.class, this::pong)
                 .build();
     }
 
@@ -40,17 +41,18 @@ class ClusterAwareActor extends AbstractLoggingActor {
     private void tick(Member member) {
         String path = member.address().toString() + self().path().toStringWithoutAddress();
         ActorSelection actorSelection = context().actorSelection(path);
-        log().debug("Ping -> {}", actorSelection);
-        actorSelection.tell("ping", self());
+        Message.Ping ping = new Message.Ping();
+        log().debug("{} -> {}", ping, actorSelection);
+        actorSelection.tell(ping, self());
     }
 
-    private void ping() {
-        log().debug("Ping <- {}", sender());
-        sender().tell("pong", self());
+    private void ping(Message.Ping ping) {
+        log().debug("{} <- {}", ping, sender());
+        sender().tell(Message.Pong.from(ping), self());
     }
 
-    private void pong() {
-        log().debug("Pong <- {}", sender());
+    private void pong(Message.Pong pong) {
+        log().debug("{} <- {}", pong, sender());
     }
 
     @Override
@@ -73,5 +75,37 @@ class ClusterAwareActor extends AbstractLoggingActor {
 
     static Props props() {
         return Props.create(ClusterAwareActor.class);
+    }
+
+    interface Message {
+        class Ping implements Serializable {
+            final long time;
+
+            Ping() {
+                time = System.nanoTime();
+            }
+
+            @Override
+            public String toString() {
+                return String.format("%s[%dus]", getClass().getSimpleName(), time);
+            }
+        }
+
+        class Pong implements Serializable {
+            final long pingTime;
+
+            private Pong(long pingTime) {
+                this.pingTime = pingTime;
+            }
+
+            static Pong from(Ping ping) {
+                return new Pong(ping.time);
+            }
+
+            @Override
+            public String toString() {
+                return String.format("%s[elapsed %.9fs, %dus]", getClass().getSimpleName(), (System.nanoTime() - pingTime) / 1000000000.0, pingTime);
+            }
+        }
     }
 }
